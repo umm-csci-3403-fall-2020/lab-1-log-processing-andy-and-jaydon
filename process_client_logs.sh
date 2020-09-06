47e371cd8ccba3410e0d0ffc522ed0808cfe4ae1
Ellis <wegle030@morris.umn.edu>
2020-09-06 12:31:37 -0500

    Quick Fix for process_client_logs

--- process_client_logs.sh
+++ process_client_logs.sh
@@ -4,9 +4,9 @@ touch /home/$USER/lab-1-log-processing-andy-and-jonah/$1/failed_login_data.txt
 
 touch /home/$USER/lab-1-log-processing-andy-and-jonah/$1/intermediate.txt
 
-grep "Failed" $1/* > tess.txt
+grep "Failed" $1/* > intermediate.txt
 
-awk '{printf "%s ",$1} {printf "%s ",$2} {split($3, a, ":"); printf "%s ",a[1]} {for (H=1;H<=NF;H++) if ($H == "user") {printf "%s ", $(H+1)};} {for (I=1;I<=NF;I++) if ($I == "from") {printf "%s\n ",$(I+1)};} ' tess.txt > $1/failed_login_data.txt
+awk '{printf "%s ",$1} {printf "%s ",$2} {split($3, a, ":"); printf "%s ",a[1]} {for (H=1;H<=NF;H++) if ($H == "user") {printf "%s ", $(H+1)};} {for (I=1;I<=NF;I++) if ($I == "from") {printf "%s\n ",$(I+1)};} ' intermediate.txt > $1/failed_login_data.txt
 
 rm $1/intermediate.txt
 
