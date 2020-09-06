f48b6341ea89c6b0dffa1bfe61f8e69038192f12
Ellis <wegle030@morris.umn.edu>
2020-09-06 12:26:29 -0500

    First properly working process_client_logs.sh

--- process_client_logs.sh
+++ process_client_logs.sh
@@ -2,4 +2,12 @@
 
 touch /home/$USER/lab-1-log-processing-andy-and-jonah/$1/failed_login_data.txt
 
-cat  $1/* >> $1/failed_login_data.txt
+touch /home/$USER/lab-1-log-processing-andy-and-jonah/$1/intermediate.txt
+
+grep "Failed" $1/* > tess.txt
+
+awk '{printf "%s ",$1} {printf "%s ",$2} {split($3, a, ":"); printf "%s ",a[1]} {for (H=1;H<=NF;H++) if ($H == "user") {printf "%s ", $(H+1)};} {for (I=1;I<=NF;I++) if ($I == "from") {printf "%s\n ",$(I+1)};} ' tess.txt > $1/failed_login_data.txt
+
+rm $1/intermediate.txt
+
+#This script gives the desired output with some extra information which i believe comes from the grep implementation.
