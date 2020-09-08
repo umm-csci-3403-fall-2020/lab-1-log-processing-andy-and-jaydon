d1fae037beab557d74a45661c1cd0e9f4b99a993
smit9025 <smit9025@morris.umn.edu>
2020-09-08 00:08:33 -0500

    Fix create_username_dist.sh Fixed create_username_dist.sh by changing how the file finds files to pull text from and parsing that, instead of reading the whole directory plus sub-directories and parsing that. Passes the bat test now

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -3,9 +3,9 @@ DIR="$1"
 FILE="username_dist"
 (cd "$DIR" || exit
 echo >> TEMP.txt #create temp file
-grep -rIhEo '\b[a-z][a-zA-Z0-9_-]*' . | sort -u | uniq -c >> TEMP.txt #Grabs text from all files in directory that match the pattern, then sorts it and puts the names into their own lines, then counts the times each name is found 
-awk 'NF {print "data.addRow([\047" $2 ", " $1"\047])"}' TEMP.txt >> TEMP1.txt) #Takes the earlier output and creates a new line using the variables defined in awk and puts the output into a file.
-./bin/wrap_contents.sh "$DIR"/TEMP1.txt html_components/"$FILE" "$FILE".html #Wrap file contents in html and create html file
+find . -name "failed_login_data.txt" | xargs grep -rIhEo '\b[a-z][-a-zA-Z0-9_]*' | sort | uniq -c >> TEMP.txt #Grabs text from all files in directory that match the pattern, then sorts it and puts the names into their own lines, then counts the times each name is found and puts it in a file
+awk 'NF {print "data.addRow([\047" $2"\047, " $1"]);"}' TEMP.txt >> TEMP1.txt) #Takes the earlier output and creates a new line using the variables defined in awk and puts the output into a file.
+./bin/wrap_contents.sh "$DIR"/TEMP1.txt html_components/"$FILE" "$FILE".html #Wrap temp file contents in html and create html file
 mv "$FILE".html "$DIR" #Moves the created html file to the given directory
-rm "$DIR"/TEMP.txt #Remove temp file
-rm "$DIR"/TEMP1.txt
+#rm "$DIR"/TEMP.txt #Remove temp files
+#rm "$DIR"/TEMP1.txt
