1ddf93fafea87cca2518850c4c52b4431853a130
smit9025 <smit9025@morris.umn.edu>
2020-09-10 22:54:31 -0500

    Create create_country_dist.sh Created create_country_dist.sh in bin for the lab. Passes the bats tests. Has one minor error for shellcheck that I don't know how to fix related to using double quotes instead of single quotes for a function. That error is supposed to be ignored for awk, but since it is in the middle of piping, it seems to be confused. Also made some sort of change to create_hours_dist.sh, I'm not sure on what it was that I did to it.

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -7,5 +7,4 @@ find . -name "failed_login_data.txt" -print0 | xargs -0 awk '{ print $3 }' | sor
 awk 'NF {print "data.addRow([\047" $2"\047, " $1"]);"}' TEMP.txt >> TEMP1.txt) #Takes the earlier output and creates a new line using the variables defined in awk and puts the output into a file.
 ./bin/wrap_contents.sh "$DIR"/TEMP1.txt html_components/"$FILE" "$FILE".html #Wrap temp file contents in html and create html file
 mv "$FILE".html "$DIR" #Moves the created html file to the given directory
-rm "$DIR"/TEMP.txt #Remove temp files
-rm "$DIR"/TEMP1.txt
+rm "$DIR"/TEMP.txt "$DIR"/TEMP1.txt
