82f444675c282eb0ad3635c2e0bcadb258904a65
smit9025 <smit9025@morris.umn.edu>
2020-09-13 21:27:41 -0500

    Add comments to assemble_report.sh and process_logs.sh I added comments to these two files so that they are easier to understand what is going on inside of them.

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 HERE=$(pwd)
 #Extraction and processing
-mkdir SCRATCH
-for f in "$@"
+mkdir SCRATCH #Create temp dir
+for f in "$@" #Start for loop for every inputted argument
 do
-	Var="$f"
-	Name=$(basename -s .tgz "$Var")
-	mkdir SCRATCH/"$Name"
-	tar -xf "$Var" -C SCRATCH/"$Name"
-	./bin/process_client_logs.sh SCRATCH
+	Var="$f" #set name for each argument
+	Name=$(basename -s .tgz "$Var") #Store file name into variable
+	mkdir SCRATCH/"$Name" #Make directory based on file name in temp dir
+	tar -xf "$Var" -C SCRATCH/"$Name" #Extract tar into proper dir inside temp dir
+	./bin/process_client_logs.sh SCRATCH #Process logs in temp dir and create proper failed_login_data.txt file
 done
-#Rest of the commands
-./bin/create_username_dist.sh SCRATCH > /dev/null
-./bin/create_hours_dist.sh SCRATCH > /dev/null
-./bin/create_country_dist.sh SCRATCH > /dev/null
-./bin/assemble_report.sh SCRATCH > /dev/null
-mv SCRATCH/failed_login_summary.html "$HERE"
+#Rest of the commands to run
+./bin/create_username_dist.sh SCRATCH > /dev/null #Create html file for usernames
+./bin/create_hours_dist.sh SCRATCH > /dev/null #Create html file for hours
+./bin/create_country_dist.sh SCRATCH > /dev/null #Create html file for countries
+./bin/assemble_report.sh SCRATCH > /dev/null #Combine html files together into main html file
+mv SCRATCH/failed_login_summary.html "$HERE" #Move main html file to top directory for analysis
