d090b628f1d70aca5a646e2c53827814975112d3
Ellis <wegle030@morris.umn.edu>
2020-09-13 20:50:58 -0500

    Fixed root error in process_client_logs

---
+++ process_client_logs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#touch "$1/failed_login_data.txt"
+#touch "$1/intermediate.txt"
+(cd "$1"
+grep --exclude="interm.txt" -rh "Failed" . >> "interm.txt"
+
+awk '{printf "%s ",$1} {printf "%s ",$2} {split($3, a, ":"); printf "%s ",a[1]} {for (H=1;H<=NF;H++) if ($H == "from") {printf "%s ", $(H-1)};} {for (I=1;I<=NF;I++) if ($I == "from") {printf "%s\n ",$(I+1)};} ' "interm.txt" >> "failed_login_data.txt" 
+rm "interm.txt"
+)
+
+#This script uses grep to pass the failed logins into an intermediate file. It then uses awk to extract what we want from the intermediate file in the correct format, and then deletes the intermediate file.
