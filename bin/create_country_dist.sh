f81d70edd7762bdf2062badc1422fda96a79a02e
smit9025 <smit9025@morris.umn.edu>
2020-09-13 21:40:34 -0500

    Beautify files I restyled the files assemble_report, create_country_dist, create_hours_dist, and create_username_dist.sh so that are better to look at by moving comments and fixing spacing

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -2,11 +2,22 @@
 DIR="$1"
 FILE="country_dist"
 (cd "$DIR" || exit
+
 # shellcheck disable=SC2016
-find . -name "failed_login_data.txt" -print0 | xargs -0 awk '{ print $5 }' | sort | tee TMP #Grabs text from all files in directory that match the pattern, then sorts it and puts the names into their own lines, then counts the times each name is found and puts it in a file
-join TMP ../etc/country_IP_map.txt | tee TMP1 #Takes the file with just the IPs in it and joins the country to the IP using the map file
-awk '{ print $2 }' TMP1 | sort | uniq -c | tee TMP2 #Takes the countries column, sorts it, and then counts the duplicates and prints it to a file
-awk 'NF {print "data.addRow([\047" $2"\047, " $1"]);"}' TMP2 | tee TMP3) #Takes the earlier output and creates a new line using the variables defined in awk and puts the output into a file.
-./bin/wrap_contents.sh "$DIR"/TMP3 html_components/"$FILE" "$FILE".html #Wrap temp file contents in html and create html file
+#Grabs text from all files in directory that match the pattern, then sorts it and puts the names into their own lines, then counts the times each name is found and puts it in a file
+find . -name "failed_login_data.txt" -print0 | xargs -0 awk '{ print $5 }' | sort | tee TMP
+
+#Takes the file with just the IPs in it and joins the country to the IP using the map file
+join TMP ../etc/country_IP_map.txt | tee TMP1
+
+#Takes the countries column, sorts it, and then counts the duplicates and prints it to a file
+awk '{ print $2 }' TMP1 | sort | uniq -c | tee TMP2
+
+#Takes the earlier output and creates a new line using the variables defined in awk and puts the output into a file.
+awk 'NF {print "data.addRow([\047" $2"\047, " $1"]);"}' TMP2 | tee TMP3)
+
+#Wrap temp file contents in html and create html file
+./bin/wrap_contents.sh "$DIR"/TMP3 html_components/"$FILE" "$FILE".html
+
 mv "$FILE".html "$DIR" #Moves the created html file to the given directory
 rm "$DIR"/TMP "$DIR"/TMP1 "$DIR"/TMP2 "$DIR"/TMP3 #Remove temp files
--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -2,10 +2,19 @@
 DIR="$1"
 FILE="hours_dist"
 (cd "$DIR" || exit
-echo >> TEMP.txt #create temp file
+#create temp file
+echo >> TEMP.txt 
+
 # shellcheck disable=SC2016
-find . -name "failed_login_data.txt" -print0 | xargs -0 awk '{ print $3 }' | sort | uniq -c >> TEMP.txt #Grabs text from all files in directory that match the pattern, then sorts it and puts the names into their own lines, then counts the times each name is found and puts it in a file
-awk 'NF {print "data.addRow([\047" $2"\047, " $1"]);"}' TEMP.txt >> TEMP1.txt) #Takes the earlier output and creates a new line using the variables defined in awk and puts the output into a file.
-./bin/wrap_contents.sh "$DIR"/TEMP1.txt html_components/"$FILE" "$FILE".html #Wrap temp file contents in html and create html file
-mv "$FILE".html "$DIR" #Moves the created html file to the given directory
+#Grabs text from all files in directory that match the pattern, then sorts it and puts the names into their own lines, then counts the times each name is found and puts it in a file
+find . -name "failed_login_data.txt" -print0 | xargs -0 awk '{ print $3 }' | sort | uniq -c >> TEMP.txt
+
+#Takes the earlier output and creates a new line using the variables defined in awk and puts the output into a file.
+awk 'NF {print "data.addRow([\047" $2"\047, " $1"]);"}' TEMP.txt >> TEMP1.txt).
+
+#Wrap temp file contents in html and create html file
+./bin/wrap_contents.sh "$DIR"/TEMP1.txt html_components/"$FILE" "$FILE".html
+
+#Moves the created html file to the given directory and remove temp files
+mv "$FILE".html "$DIR"
 rm "$DIR"/TEMP.txt "$DIR"/TEMP1.txt
